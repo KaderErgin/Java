585891f7ca30fe2fd2a7a58ef00ebdc8d7a7f68b
Kader Ergin <kader.ergin.37@gmail.com>
2023-11-27 23:50:11 +0300

    Create finding_leap_year.java

---
+++ finding_leap_year/finding_leap_year.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class findingLeapYear {
+    public static void main(String[] args) {
+
+        Scanner inp = new Scanner(System.in);
+
+        int yil;
+
+        System.out.print("Yıl Giriniz : ");
+        yil = inp.nextInt();
+
+        if ((yil % 4 == 0) || (yil % 400 == 0)) {
+            System.out.println(yil + " bir artık yıldır ! ");
+        } else {
+            System.out.println(yil + " bir artık yıl değildir ! ");
+        }
+    }
+}
